77ce96aa28b97ca4688798feac2e1d04636074b8
soyjang <soyjang1925@gmail.com>
2026-04-06 17:08:03 +0900

    revised

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,45 @@ $(document).ready(function() {
         $(this).addClass('d-none');
     });
 });
+
+
+$(document).ready(function() {
+    var counted = 0;
+
+    $(window).on('scroll', function() {
+        // 컨테이너가 화면 하단에 걸칠 때 시작하도록 계산
+        var counterOffset = $('.info-counter-container').offset().top;
+        var windowHeight = window.innerHeight;
+        var scrollTop = $(window).scrollTop();
+
+        // 숫자가 아직 카운트되지 않았고, 스크롤 위치가 컨테이너 근처에 왔을 때
+        if (counted == 0 && scrollTop > (counterOffset - windowHeight + 100)) {
+            $('.counter').each(function() {
+                var $this = $(this);
+                var countTo = parseInt($this.attr('data-count')); // HTML의 data-count 값 가져오기
+
+                $({ countNum: 0 }).animate({
+                    countNum: countTo
+                }, {
+                    duration: 2000,
+                    easing: 'swing',
+                    step: function() {
+                        $this.text(Math.floor(this.countNum));
+                    },
+                    complete: function() {
+                        // 최종 수치 뒤에 기호 붙이기
+                        var val = this.countNum;
+                        if (val == 580) {
+                            $this.text(val + "kL");
+                        } else if (val == 300 || val == 180) {
+                            $this.text(val + "+");
+                        } else {
+                            $this.text(val);
+                        }
+                    }
+                });
+            });
+            counted = 1; // 중복 실행 방지
+        }
+    });
+});
